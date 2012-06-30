7d6f0517cdc3b8ec700b9f7b27d42cac04894d86
Andrew Oates <andrew@andrewoates.com>
2012-06-30 12:28:00 -0400

    added line-length tracking to vterm

--- dev/video/vterm.c
+++ dev/video/vterm.c
@@ -26,6 +26,9 @@ struct vterm {
 
   // Cache the video's dimensions.
   uint32_t vwidth, vheight;
+
+  // The line-length of each line on the display.
+  uint32_t* line_length;
 };
 
 // Scroll down a given nmuber of lines.
@@ -40,6 +43,12 @@ static void scroll(vterm_t* t, int amt) {
       }
       video_setc(t->video, row, col, newc);
     }
+
+    uint32_t new_length = 0;
+    if (row + amt < t->vheight) {
+      new_length = t->line_length[row + amt];
+    }
+    t->line_length[row] = new_length;
   }
   t->cursor_y -= amt;
 }
@@ -50,6 +59,13 @@ vterm_t* vterm_create(video_t* v) {
   term->cursor_x = term->cursor_y = 0;
   term->vwidth = video_get_width(v);
   term->vheight = video_get_height(v);
+
+  term->line_length = (uint32_t*)kmalloc(
+      sizeof(uint32_t) * term->vheight);
+  for (uint32_t i = 0; i < term->vheight; i++) {
+    term->line_length[i] = 0;
+  }
+
   return term;
 }
 
@@ -57,15 +73,24 @@ void vterm_putc(vterm_t* t, uint8_t c) {
   // First calculate new cursor position if needed.
   if (c == '\r') {
     t->cursor_x = 0;
+    t->line_length[t->cursor_y] = 0;
   } else if (c == '\f') {
+    t->line_length[t->cursor_y] = t->cursor_x;
     t->cursor_y++;
   } else if (c == '\n') {
     // TODO(aoates): do we want to handle '\n'?
+    t->line_length[t->cursor_y] = t->cursor_x;
     t->cursor_x = 0;
     t->cursor_y++;
   } else if (c == '\b') {
     if (t->cursor_x == 0 && t->cursor_y > 0) {
-      t->cursor_x = t->vwidth - 1;
+      if (t->line_length[t->cursor_y - 1] < t->vwidth) {
+        t->cursor_x = t->line_length[t->cursor_y - 1];
+      } else {
+        // If the last line was full, just immediately delete the last character
+        // on it.
+        t->cursor_x = t->vwidth - 1;
+      }
       t->cursor_y--;
     } else if (t->cursor_x > 0) {
       t->cursor_x--;
@@ -77,8 +102,9 @@ void vterm_putc(vterm_t* t, uint8_t c) {
     t->cursor_x++;
   }
 
-  // Wrap to nenxt line if needed.
+  // Wrap to next line if needed.
   if (t->cursor_x >= t->vwidth) {
+    t->line_length[t->cursor_y] = t->cursor_x;
     t->cursor_x = 0;
     t->cursor_y++;
   }
