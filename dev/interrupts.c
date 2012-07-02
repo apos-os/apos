9f3adbdc075322db49a20aa07b7cc272e6f1abd6
Andrew Oates <andrew@andrewoates.com>
2012-07-02 15:01:03 -0400

    interrupt cleanup: remove irqX decls

--- dev/interrupts.c
+++ dev/interrupts.c
@@ -43,23 +43,6 @@ extern void int17();
 extern void int18();
 extern void int19();
 
-extern void irq0();
-extern void irq1();
-extern void irq2();
-extern void irq3();
-extern void irq4();
-extern void irq5();
-extern void irq6();
-extern void irq7();
-extern void irq8();
-extern void irq9();
-extern void irq10();
-extern void irq11();
-extern void irq12();
-extern void irq13();
-extern void irq14();
-extern void irq15();
-
 void register_interrupt_handler(uint8_t num, int_handler_t h) {
   KASSERT(idt != 0);
   KASSERT(num < idt_entries);
