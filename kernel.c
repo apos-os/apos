0c28c9f5e825902f1ec132920ade5fe2888e5529
Andrew Oates <andrew@andrewoates.com>
2012-07-04 20:07:21 -0400

    kmain: run malloc tests by default

--- kernel.c
+++ kernel.c
@@ -121,8 +121,8 @@ void kmain(memory_info_t* meminfo) {
   print("\nmeminfo->phys_map_start:    0x"); print(utoa_hex(meminfo->phys_map_start));
   vterm_clear(g_vterm);
 
-  kthread_test();
-  //kmalloc_test();
+  //kthread_test();
+  kmalloc_test();
   // interrupt_clobber_test();
 
   //print("\n");
