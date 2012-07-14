f046407f4852cf7421e8b7ff71cea21622674c0c
Andrew Oates <andrew@andrewoates.com>
2012-07-13 22:02:11 -0400

    vfs: add file_t struct

---
+++ vfs/file.c
@@ -0,0 +1,21 @@
+// Copyright 2014 Andrew Oates.  All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "vfs/file.h"
+
+void file_init_file(file_t* f) {
+  f->vnode = 0x0;
+  f->refcount = 0;
+  f->pos = 0;
+}
