9267bd8647cda012065a8848bfb46ef71c36fb78
Andrew Oates <andrew@andrewoates.com>
2012-08-07 00:27:14 -0400

    uhci: add 'uhci ls' command and fix test command

--- dev/usb/uhci/uhci_cmd.c
+++ dev/usb/uhci/uhci_cmd.c
@@ -12,25 +12,131 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "common/io.h"
 #include "common/kstring.h"
+#include "dev/interrupts.h"
 #include "dev/usb/hcd.h"
 #include "dev/usb/uhci/uhci-internal.h"
+#include "dev/usb/uhci/uhci_registers.h"
 #include "dev/usb/uhci/uhci.h"
 #include "dev/usb/usb.h"
 #include "kshell.h"
+#include "util/flag_printf.h"
 
-// Test the UHCI controller(s).
-void uhci_cmd(int argc, char* argv[]) {
-  if (argc != 3) {
-    ksh_printf("usage: uhci <idx> <port>\n");
+static flag_spec_t USBCMD_FLAGS[] = {
+  { 0x0080, "MAXP" },
+  { 0x0040, "CF" },
+  { 0x0020, "SWDBG" },
+  { 0x0010, "FGR" },
+  { 0x0008, "EGSM" },
+  { 0x0004, "GRESET" },
+  { 0x0002, "HCRESET" },
+  { 0x0001, "RS" },
+  { 0x0, 0x0 },
+};
+
+static flag_spec_t USBSTS_FLAGS[] = {
+  { 0x0020, "HALTED" },
+  { 0x0010, "HCERROR" },
+  { 0x0008, "HSYSERROR" },
+  { 0x0004, "RESUME" },
+  { 0x0002, "ERRINT" },
+  { 0x0001, "INT" },
+  { 0x0, 0x0 },
+};
+
+static flag_spec_t USBINTR_FLAGS[] = {
+  { 0x0008, "SHORTP" },
+  { 0x0004, "IOC" },
+  { 0x0002, "RESUME" },
+  { 0x0001, "TMO_CRC" },
+  { 0x0, 0x0 },
+};
+
+static flag_spec_t PORTSC_FLAGS[] = {
+  { 0x1000, "SUSPEND" },
+  { 0x0200, "RST" },
+  { 0x0100, "LOSPEED" },
+  { 0x0040, "RESUME_DTCT" },
+  { 0x0030, "LINE_STATUS" },
+  { 0x0008, "ENABLE_CHG" },
+  { 0x0004, "ENABLE" },
+  { 0x0002, "CONNECT_CHG" },
+  { 0x0001, "CONNECT" },
+  { 0x0, 0x0 },
+};
+
+static void uhci_cmd_test(int argc, char* argv[]) {
+  if (argc != 4) {
+    ksh_printf("usage: uhci test <controller idx> <port>\n");
     return;
   }
-  int idx = atoi(argv[1]);
-  int port = atoi(argv[2]);
+  int idx = atoi(argv[2]);
+  int port = atoi(argv[3]);
   if (idx >= usb_num_host_controllers()) {
     ksh_printf("error: invalid controller %d\n", idx);
     return;
   }
   // Test the USB controller.
   uhci_test_controller(usb_get_host_controller(idx), port);
+}
+
+static void uhci_cmd_ls(int argc, char* argv[]) {
+  if (argc != 2 && argc != 3) {
+    ksh_printf("usage: uhci ls [idx]\n");
+    return;
+  }
+  PUSH_AND_DISABLE_INTERRUPTS();
+  if (argc == 2) {
+    // FIXME(aoates): this will break when we add other controller types!
+    for (int i = 0; i < usb_num_host_controllers(); ++i) {
+      usb_uhci_t* hc = (usb_uhci_t*)usb_get_host_controller(i)->dev_data;
+      ksh_printf("USB %d: port: 0x%x\n", i, hc->base_port);
+    }
+  } else {
+    int idx = atoi(argv[2]);
+    if (idx >= usb_num_host_controllers()) {
+      ksh_printf("error: invalid controller %d\n", idx);
+      return;
+    }
+    usb_uhci_t* hc = (usb_uhci_t*)usb_get_host_controller(idx)->dev_data;
+
+    // Get the current state.
+    uint16_t usbcmd = ins(hc->base_port + USBCMD);
+    uint16_t usbsts = ins(hc->base_port + USBSTS);
+    uint16_t usbintr = ins(hc->base_port + USBINTR);
+    uint16_t frnum = ins(hc->base_port + FRNUM);
+    uint32_t flbaseaddr = inl(hc->base_port + FLBASEADDR);
+    uint8_t sof_modify = inb(hc->base_port + SOF_MODIFY);
+    uint16_t portsc1 = ins(hc->base_port + PORTSC1);
+    uint16_t portsc2 = ins(hc->base_port + PORTSC2);
+
+    // Print the registers.
+    char buf[1024];
+    ksh_printf("USB %d: port: 0x%x\n", idx, hc->base_port);
+    flag_sprintf(buf, usbcmd, USBCMD_FLAGS); ksh_printf("  USBCMD: %s\n", buf);
+    flag_sprintf(buf, usbsts, USBSTS_FLAGS); ksh_printf("  USBSTS: %s\n", buf);
+    flag_sprintf(buf, usbintr, USBINTR_FLAGS); ksh_printf("  USBINTR: %s\n", buf);
+    ksh_printf("  FRNUM: 0x%x\n", frnum);
+    ksh_printf("  FLBASEADDR: 0x%x\n", flbaseaddr);
+    ksh_printf("  SOF_MODIFY: 0x%x\n", sof_modify);
+    flag_sprintf(buf, portsc1, PORTSC_FLAGS); ksh_printf("  PORTSC1: %s\n", buf);
+    flag_sprintf(buf, portsc2, PORTSC_FLAGS); ksh_printf("  PORTSC2: %s\n", buf);
+  }
+  POP_INTERRUPTS();
+}
+
+// Test the UHCI controller(s).
+void uhci_cmd(int argc, char* argv[]) {
+  if (argc < 2) {
+    ksh_printf("usage: uhci <cmd> <args> ...\n");
+    return;
+  }
+  if (kstrcmp(argv[1], "test") == 0) {
+    uhci_cmd_test(argc, argv);
+  } else if (kstrcmp(argv[1], "ls") == 0) {
+    uhci_cmd_ls(argc, argv);
+  } else {
+    ksh_printf("error: unknown command '%s'\n", argv[1]);
+  }
 }
---
+++ dev/usb/uhci/uhci_registers.h
@@ -0,0 +1,68 @@
+// Copyright 2014 Andrew Oates.  All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Register offsets and flag/mask values for the UHCI controller ports.
+#ifndef APOO_DEV_USB_UHCI_UHCI_REGISTERS_H
+#define APOO_DEV_USB_UHCI_UHCI_REGISTERS_H
+
+// Number of frames in the frame list.
+#define UHCI_NUM_FRAMES 1024
+
+// UHCI I/O Registers (offsets from the base).
+#define USBCMD     0x00  // 16 bits
+#define USBSTS     0x02  // 16 bits
+#define USBINTR    0x04  // 16 bits
+#define FRNUM      0x06  // 16 bits
+#define FLBASEADDR 0x08  // 32 bits
+#define SOF_MODIFY 0x0C  // 8 bits
+#define PORTSC1    0x10  // 16 bits
+#define PORTSC2    0x12  // 16 bits
+
+// Bits within those registers.
+#define USBCMD_MAXP    0x0080  // Max packet (1=64 bytes, 0=32 bytes)
+#define USBCMD_CF      0x0040  // Configure flag (set by software)
+#define USBCMD_SWDBG   0x0020  // Software debug
+#define USBCMD_FGR     0x0010  // Force global resume
+#define USBCMD_EGSM    0x0008  // Enter global suspend mode
+#define USBCMD_GRESET  0x0004  // Global reset
+#define USBCMD_HCRESET 0x0002  // Host controller reset
+#define USBCMD_RS      0x0001  // Run/stop
+
+#define USBSTS_HALTED     0x0020  // Host controller halted
+#define USBSTS_HCERROR    0x0010  // Host controller process error
+#define USBSTS_HSYSERROR  0x0008  // Host system (e.g. PCI) error
+#define USBSTS_RESUME     0x0004  // Resume detect
+#define USBSTS_ERRINT     0x0002  // USB error interrupt
+#define USBSTS_INT        0x0001  // USB interrupt (IOC, SPD, etc)
+
+#define USBINTR_SHORTP    0x0008  // Short packet interrupt enable
+#define USBINTR_IOC       0x0004  // Interrupt on complete interrupt enable
+#define USBINTR_RESUME    0x0002  // Resume interrupt enable
+#define USBINTR_TMO_CRC   0x0001  // Timeout/CRC interrupt enable
+
+#define FRNUM_MASK  0x07FF  // Only bits 10:0 are used for the frnum
+#define FLBASEADDR_MASK 0xFFFFF000  // Must be page-aligned
+
+// Port status/control bits.  One set each for port1 and port2.
+#define PORTSC_SUSPEND     0x1000  // R/W (1=suspended)
+#define PORTSC_RST         0x0200  // R/W - Port reset
+#define PORTSC_LOSPEED     0x0100  // R/O - Low speed device attached
+#define PORTSC_RESUME_DTCT 0x0040  // R/W - Resume detect enable
+#define PORTSC_LINE_STATUS 0x0030  // R/O - Line status bits
+#define PORTSC_ENABLE_CHG  0x0008  // R/WC - Enable/disable status change
+#define PORTSC_ENABLE      0x0004  // R/W - Enable/disable port
+#define PORTSC_CONNECT_CHG 0x0002  // R/WC - Connect status change
+#define PORTSC_CONNECT     0x0001  // R/O - Connect status (1=device connected)
+
+#endif
