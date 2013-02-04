1fda3441e2a1a525a0f1cc3bc1b80633b2b99b8b
Andrew Oates <andrew@andrewoates.com>
2013-02-03 19:56:54 -0500

    kthread: fix unused function warning

--- proc/kthread.h
+++ proc/kthread.h
@@ -116,11 +116,11 @@ void kmutex_assert_is_not_held(kmutex_t* m);
 //    // x->lock is automatically unlocked here when my_x_lock goes out of
 //    // scope.
 //  }
-static kmutex_t* _kmutex_autolock_lock(kmutex_t* m) {
+static inline kmutex_t* _kmutex_autolock_lock(kmutex_t* m) {
   kmutex_lock(m);
   return m;
 }
-static void _kmutex_autolock_unlock(kmutex_t** m) {
+static inline void _kmutex_autolock_unlock(kmutex_t** m) {
   kmutex_unlock(*m);
 }
 #define KMUTEX_AUTO_LOCK(name, lock) \
