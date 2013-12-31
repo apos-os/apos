a192e8534cb459518ead720a20420c9878685b2b
Andrew Oates <andrew@andrewoates.com>
2013-12-31 16:51:51 -0500

    usb: add driver names

--- dev/usb/drivers/drivers.h
+++ dev/usb/drivers/drivers.h
@@ -26,6 +26,9 @@ typedef struct {
   // Adopt the given device, returning -errno on error.  If this is called,
   // check_device will have been previously called with the same device.
   int (*adopt_device)(usb_device_t*);
+
+  // Human-readable name of the driver.
+  const char* name;
 } usb_driver_t;
 
 // Finds a driver for the given device, or returns NULL if none can be found.
