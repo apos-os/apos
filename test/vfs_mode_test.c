7884196f4f9a055daafd1551bfef3994bf5bcd74
Andrew Oates <andrew@andrewoates.com>
2014-05-12 21:03:26 -0400

    vfs: add test for check mode in lstat()

--- test/vfs_mode_test.c
+++ test/vfs_mode_test.c
@@ -531,6 +531,37 @@ static void do_syscall_mode_test(void* arg) {
   KEXPECT_EQ(0, vfs_chdir(orig_cwd));
 
 
+  // lstat()
+  apos_stat_t stat;
+  KTEST_BEGIN("vfs mode test: vfs_lstat() succeeds in non-readable directory");
+  create_file("syscall_mode_test/no_read/f", "---------");
+  KEXPECT_EQ(0, vfs_lstat("syscall_mode_test/no_read/f", &stat));
+  KEXPECT_EQ(0, vfs_unlink("syscall_mode_test/no_read/f"));
+
+  KTEST_BEGIN("vfs mode test: vfs_lstat() succeeds in non-writable directory");
+  KEXPECT_EQ(0,
+             vfs_lchmod("syscall_mode_test/no_write", kNoWrite | VFS_S_IWUSR));
+  create_file("syscall_mode_test/no_write/f", "---------");
+  KEXPECT_EQ(0, vfs_lchmod("syscall_mode_test/no_write", kNoWrite));
+
+  KEXPECT_EQ(0, vfs_lstat("syscall_mode_test/no_write/f", &stat));
+
+  KEXPECT_EQ(0,
+             vfs_lchmod("syscall_mode_test/no_write", kNoWrite | VFS_S_IWUSR));
+  KEXPECT_EQ(0, vfs_unlink("syscall_mode_test/no_write/f"));
+  KEXPECT_EQ(0, vfs_lchmod("syscall_mode_test/no_write", kNoWrite));
+
+  KTEST_BEGIN("vfs mode test: vfs_lstat() fails in non-executable directory");
+  KEXPECT_EQ(0, vfs_lchmod("syscall_mode_test/no_exec", kNoExec | VFS_S_IXUSR));
+  create_file("syscall_mode_test/no_exec/f", "---------");
+  KEXPECT_EQ(0, vfs_lchmod("syscall_mode_test/no_exec", kNoExec));
+
+  KEXPECT_EQ(-EACCES, vfs_lstat("syscall_mode_test/no_exec/f", &stat));
+  KEXPECT_EQ(0, vfs_lchmod("syscall_mode_test/no_exec", kNoExec | VFS_S_IXUSR));
+  KEXPECT_EQ(0, vfs_unlink("syscall_mode_test/no_exec/f"));
+  KEXPECT_EQ(0, vfs_lchmod("syscall_mode_test/no_exec", kNoExec));
+
+
   // Teardown.
   KTEST_BEGIN("vfs mode test: syscall mode test cleanup");
   KEXPECT_EQ(0, vfs_get_vnode_refcount_for_path("syscall_mode_test/no_read"));
