28ac659f62aa702099da6e727b76ffb99f3c6daa
Andrew Oates <andrew@andrewoates.com>
2014-08-02 17:55:36 -0400

    page_alloc i586: type cleanup (uint32_t -> addr_t, pde_t, etc)

--- archs/i586/arch/memory/page_alloc.c
+++ archs/i586/arch/memory/page_alloc.c
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <stddef.h>
 #include <stdint.h>
 
 #include "arch/memory/layout.h"
@@ -24,22 +25,25 @@
 
 #define SUPPORTS_INVPLG_INSTRUCTION 0
 
+typedef addr_t pte_t;
+typedef addr_t pde_t;
+
 // The current stack of free page frame addresses.  The stack is guarded on both
 // ends by an invalid (non-aligned) page frames.
-static uint32_t* free_frame_stack = 0;
-static uint32_t stack_size = -1;
-static uint32_t stack_idx = -1;  // points to the next free element on the stack.
+static phys_addr_t* free_frame_stack = 0;
+static size_t stack_size = -1;
+static size_t stack_idx = -1;  // points to the next free element on the stack.
 
 // Initialize the allocator with the given meminfo.
 void page_frame_alloc_init(memory_info_t* meminfo) {
-  const uint32_t total_frames =
+  const size_t total_frames =
       (meminfo->lower_memory + meminfo->upper_memory) / PAGE_SIZE;
   // Get the first free frame address after the kernel.
-  const uint32_t kernel_end_page = next_page(meminfo->kernel_end_phys);
+  const phys_addr_t kernel_end_page = next_page(meminfo->kernel_end_phys);
 
   // Take all the frames above what the kernel is already using.  Don't include
   // frames before the kernel (<1MB).
-  const uint32_t free_frames = total_frames - (kernel_end_page / PAGE_SIZE);
+  const size_t free_frames = total_frames - (kernel_end_page / PAGE_SIZE);
 
   // Allocate a stack of the appropriate size.  We need 4 bytes per free frame,
   // plus 8 bytes for guard addresses.  Round up to use an even number of pages
@@ -48,18 +52,18 @@ void page_frame_alloc_init(memory_info_t* meminfo) {
   stack_size = next_page(stack_size); // round up.
 
   // The stack will live directly above the kernel (at the next page boundary).
-  free_frame_stack = (uint32_t*)next_page(meminfo->kernel_end_virt);
+  free_frame_stack = (phys_addr_t*)next_page(meminfo->kernel_end_virt);
 
   // Fill the stack with crap.
-  for (uint32_t i = 0; i < stack_size; ++i) {
+  for (size_t i = 0; i < stack_size; ++i) {
     free_frame_stack[i] = 0xDEADBEEF;
   }
 
   // Push each free frame onto the stack.  Don't count the frames we just used
   // for the stack, though.
   stack_idx = 0;
-  uint32_t address = kernel_end_page + stack_size;
-  for (uint32_t i = 0; i < free_frames - (stack_size / PAGE_SIZE); ++i) {
+  phys_addr_t address = kernel_end_page + stack_size;
+  for (size_t i = 0; i < free_frames - (stack_size / PAGE_SIZE); ++i) {
     KASSERT(is_page_aligned(address));
     KASSERT(address + PAGE_SIZE <= total_frames * PAGE_SIZE);
 
@@ -73,12 +77,12 @@ phys_addr_t page_frame_alloc() {
     return 0;
   }
 
-  uint32_t frame = free_frame_stack[--stack_idx];
+  phys_addr_t frame = free_frame_stack[--stack_idx];
 
   if (ENABLE_KERNEL_SAFETY_NETS) {
     // Fill the page with crap.
-    uint32_t virt_frame = phys2virt(frame);
-    for (int i = 0; i < PAGE_SIZE / 4; ++i) {
+    addr_t virt_frame = phys2virt(frame);
+    for (size_t i = 0; i < PAGE_SIZE / sizeof(uint32_t); ++i) {
       ((uint32_t*)virt_frame)[i] = 0xCAFEBABE;
     }
   }
@@ -92,13 +96,13 @@ void page_frame_free(phys_addr_t frame_addr) {
 
   if (ENABLE_KERNEL_SAFETY_NETS) {
     // Check that the page frame isn't already free.
-    for (uint32_t i = 0; i < stack_idx; ++i) {
+    for (size_t i = 0; i < stack_idx; ++i) {
       KASSERT(free_frame_stack[i] != frame_addr);
     }
 
     // Fill the page with crap.
-    uint32_t virt_frame = phys2virt(frame_addr);
-    for (int i = 0; i < PAGE_SIZE / 4; ++i) {
+    addr_t virt_frame = phys2virt(frame_addr);
+    for (size_t i = 0; i < PAGE_SIZE / sizeof(uint32_t); ++i) {
       ((uint32_t*)virt_frame)[i] = 0xDEADBEEF;
     }
   }
@@ -106,8 +110,7 @@ void page_frame_free(phys_addr_t frame_addr) {
   page_frame_free_nocheck(frame_addr);
 }
 
-void page_frame_free_nocheck(phys_addr_t frame) {
-  const uint32_t frame_addr = (uint32_t)frame;
+void page_frame_free_nocheck(phys_addr_t frame_addr) {
   KASSERT(is_page_aligned(frame_addr));
   KASSERT(stack_idx <= stack_size);
 
@@ -115,19 +118,20 @@ void page_frame_free_nocheck(phys_addr_t frame) {
 }
 
 // Returns the current page directory.
-static inline uint32_t* get_page_directory(void) {
-  return (uint32_t*)0xFFFFF000;
+static inline pde_t* get_page_directory(void) {
+  return (pde_t*)0xFFFFF000;
 }
 
 // Returns the page table entry for the given page address.  Requires that the
 // appropriate page table already exists and is pointed to by the page
 // directory.
-static inline uint32_t* get_page_table_entry(uint32_t virt) {
-  return (uint32_t*)0xFFC00000 + (virt / PAGE_SIZE);
+static inline pte_t* get_page_table_entry(addr_t virt) {
+  return (pte_t*)0xFFC00000 + (virt / PAGE_SIZE);
 }
 
 // Invalidate the TLB entry for the given virtual address.
-static inline void invalidate_tlb(uint32_t virt) {
+static inline void invalidate_tlb(addr_t virt) {
+  _Static_assert(sizeof(virt) == sizeof(uint32_t), "Not 32-bit");
   if (SUPPORTS_INVPLG_INSTRUCTION) {
     asm volatile (
         "invlpg %0\n\t"
@@ -143,10 +147,10 @@ static inline void invalidate_tlb(uint32_t virt) {
 // Given a virtual page address, returns a pointer to the page table entry
 // responsible for page.  If create is non-zero, and the page table doesn't
 // exist, a page table is allocated and initialized for it.
-static uint32_t* get_or_create_page_table_entry(uint32_t virt, int create) {
-  uint32_t* page_directory = get_page_directory();
-  const uint32_t page_idx = virt / PAGE_SIZE;
-  const uint32_t page_table_idx = page_idx / PTE_NUM_ENTRIES;
+static pte_t* get_or_create_page_table_entry(addr_t virt, bool create) {
+  pde_t* page_directory = get_page_directory();
+  const size_t page_idx = virt / PAGE_SIZE;
+  const size_t page_table_idx = page_idx / PTE_NUM_ENTRIES;
 
   if (page_directory[page_table_idx] & PDE_PRESENT) {
     return get_page_table_entry(virt);
@@ -164,9 +168,9 @@ static uint32_t* get_or_create_page_table_entry(uint32_t virt, int create) {
 
     // Initialize the new page table.  Get the *first* address of the new page
     // table.
-    uint32_t* pte_virt_addr = get_page_table_entry(
+    pte_t* pte_virt_addr = get_page_table_entry(
         page_table_idx * PTE_NUM_ENTRIES * PAGE_SIZE);
-    for (uint32_t i = 0; i < PTE_NUM_ENTRIES; ++i) {
+    for (size_t i = 0; i < PTE_NUM_ENTRIES; ++i) {
       pte_virt_addr[i] = 0;
     }
 
@@ -185,7 +189,7 @@ void page_frame_map_virtual(addr_t virt, phys_addr_t phys, int prot,
 
   // The PDE entry we may create will automatically get the most permissive
   // flags.
-  uint32_t* pte = get_or_create_page_table_entry(virt, 1);
+  pte_t* pte = get_or_create_page_table_entry(virt, true);
   *pte = phys | PTE_PRESENT;
   if (prot & MEM_PROT_WRITE) *pte |= PTE_WRITABLE;
   if (access == MEM_ACCESS_KERNEL_AND_USER) *pte |= PTE_USER_ACCESS;
@@ -200,8 +204,8 @@ void page_frame_unmap_virtual(addr_t virt) {
 void page_frame_unmap_virtual_range(addr_t virt, addrdiff_t length) {
   KASSERT(virt % PAGE_SIZE == 0);
   KASSERT(length % PAGE_SIZE == 0);
-  for (uint32_t i = 0; i < length / PAGE_SIZE; ++i) {
-    uint32_t* pte = get_or_create_page_table_entry(virt + i * PAGE_SIZE, 0);
+  for (size_t i = 0; i < length / PAGE_SIZE; ++i) {
+    pte_t* pte = get_or_create_page_table_entry(virt + i * PAGE_SIZE, false);
     if (pte) {
       // Mark the page as non-present.
       *pte &= ~PTE_PRESENT;
@@ -211,12 +215,12 @@ void page_frame_unmap_virtual_range(addr_t virt, addrdiff_t length) {
 }
 
 page_dir_ptr_t page_frame_alloc_directory() {
-  uint32_t dir_phys = page_frame_alloc();
+  phys_addr_t dir_phys = page_frame_alloc();
   KASSERT(dir_phys);
   KASSERT_DBG((dir_phys & PDE_ADDRESS_MASK) == dir_phys);
 
-  uint32_t* dir = (uint32_t*)phys2virt(dir_phys);
-  for (int i = 0; i < PDE_NUM_ENTRIES; ++i) {
+  pde_t* dir = (pde_t*)phys2virt(dir_phys);
+  for (size_t i = 0; i < PDE_NUM_ENTRIES; ++i) {
     dir[i] = 0;
   }
 
@@ -241,12 +245,12 @@ void page_frame_init_global_mapping(addr_t addr, addr_t length) {
   // their page directories point to the same ones.
   // TODO(aoates): this will have overflow issues if mapping at the end of the
   // address space.
-  for (uint32_t pt_addr = addr; pt_addr < addr + length;
+  for (addr_t pt_addr = addr; pt_addr < addr + length;
        pt_addr += MIN_GLOBAL_MAPPING_SIZE) {
     // pt_addr is the first address of the region represented by each page
     // table.
     // TODO(aoates): set the GLOBAL flag on each of these mappings.
-    get_or_create_page_table_entry(pt_addr, 1);
+    get_or_create_page_table_entry(pt_addr, true);
   }
 }
 
@@ -258,12 +262,12 @@ void page_frame_link_global_mapping(page_dir_ptr_t target,
   KASSERT(addr % MIN_GLOBAL_MAPPING_SIZE == 0);
   KASSERT(length % MIN_GLOBAL_MAPPING_SIZE == 0);
 
-  uint32_t* source = get_page_directory();
-  uint32_t* target_virt = (uint32_t*)phys2virt(target);
+  pde_t* source = get_page_directory();
+  pde_t* target_virt = (pde_t*)phys2virt(target);
 
   // TODO(aoates): this will have overflow issues if mapping at the end of the
   // address space.
-  for (uint32_t pde_idx = addr / MIN_GLOBAL_MAPPING_SIZE;
+  for (size_t pde_idx = addr / MIN_GLOBAL_MAPPING_SIZE;
        pde_idx < (addr + length) / MIN_GLOBAL_MAPPING_SIZE;
        pde_idx++) {
     // TODO(aoates): do we want to to check or reset any of the flags in the
