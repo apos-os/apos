a63e4af5d53be8415aa230e7ee0a1d7801a06463
Andrew Oates <andrew@andrewoates.com>
2014-11-29 16:53:52 -0500

    vterm: store and restore character attributes

--- dev/video/vterm.c
+++ dev/video/vterm.c
@@ -24,15 +24,42 @@ struct vterm {
   video_t* video;
   int cursor_x, cursor_y;
 
+  // Current video attributes.
+  video_attr_t cattr;
+
   // Cache the video's dimensions.
   int vwidth, vheight;
 
   // The text of each line on the display.
-  uint8_t** line_text;
+  uint16_t** line_text;
 
   // The line-length of each line on the display.
   int* line_length;
 };
+
+__attribute__((always_inline))
+static inline uint8_t line_text_c(vterm_t* t, int row, int col) {
+  return t->line_text[row][col] & 0xFF;
+}
+
+__attribute__((always_inline))
+static inline uint8_t line_text_attr(vterm_t* t, int row, int col) {
+  return (t->line_text[row][col] & 0xFF00) >> 8;
+}
+
+__attribute__((always_inline))
+static inline void set_line_text(vterm_t* t, int row, int col,
+                                 uint8_t c, video_attr_t attr) {
+  t->line_text[row][col] = c | (attr << 8);
+}
+
+// Sets a character in the video and in the vterm's stored version.
+__attribute__((always_inline))
+static inline void vterm_setc(vterm_t* t, int row, int col,
+                              uint8_t c, video_attr_t attr) {
+  video_setc(t->video, row, col, c, attr);
+  set_line_text(t, row, col, c, attr);
+}
 
 // Scroll down a given nmuber of lines.
 static void scroll(vterm_t* t, int amt) {
@@ -41,11 +68,12 @@ static void scroll(vterm_t* t, int amt) {
   for (int row = 0; row < t->vheight; row++) {
     for (int col = 0; col < t->vwidth; col++) {
       uint8_t newc = ' ';
+      video_attr_t new_attr = t->cattr;
       if (row + amt < t->vheight) {
         newc = video_getc(t->video, row + amt, col);
+        new_attr = video_get_attr(t->video, row + amt, col);
       }
-      video_setc(t->video, row, col, newc, VGA_DEFAULT_ATTR);
-      t->line_text[row][col] = newc;
+      vterm_setc(t, row, col, newc, new_attr);
     }
 
     int new_length = 0;
@@ -63,30 +91,24 @@ vterm_t* vterm_create(video_t* v) {
   term->cursor_x = term->cursor_y = 0;
   term->vwidth = video_get_width(v);
   term->vheight = video_get_height(v);
+  term->cattr = VGA_DEFAULT_ATTR;
 
   term->line_length = (int*)kmalloc(sizeof(int) * term->vheight);
   for (int i = 0; i < term->vheight; i++) {
     term->line_length[i] = 0;
   }
 
-  term->line_text = (uint8_t**)kmalloc(
-      sizeof(uint8_t*) * term->vheight);
+  term->line_text = (uint16_t**)kmalloc(
+      sizeof(uint16_t*) * term->vheight);
   for (int i = 0; i < term->vheight; i++) {
-    term->line_text[i] = (uint8_t*)kmalloc(
-        sizeof(uint8_t) * term->vwidth);
+    term->line_text[i] = (uint16_t*)kmalloc(
+        sizeof(uint16_t) * term->vwidth);
     for (int j = 0; j < term->vwidth; j++) {
-      term->line_text[i][j] = ' ';
+      set_line_text(term, i, j, ' ', term->cattr);
     }
   }
 
   return term;
-}
-
-// Sets a character in the video and in the vterm's stored version.
-static inline void vterm_setc(vterm_t* t, int row,
-                              int col, uint8_t c) {
-  video_setc(t->video, row, col, c, VGA_DEFAULT_ATTR);
-  t->line_text[row][col] = c;
 }
 
 void vterm_putc(vterm_t* t, uint8_t c) {
@@ -115,10 +137,10 @@ void vterm_putc(vterm_t* t, uint8_t c) {
     } else if (t->cursor_x > 0) {
       t->cursor_x--;
     }
-    vterm_setc(t, t->cursor_y, t->cursor_x, ' ');
+    vterm_setc(t, t->cursor_y, t->cursor_x, ' ', t->cattr);
   } else {
     // Printable character.
-    vterm_setc(t, t->cursor_y, t->cursor_x, c);
+    vterm_setc(t, t->cursor_y, t->cursor_x, c, t->cattr);
     t->cursor_x++;
   }
 
@@ -136,9 +158,10 @@ void vterm_putc(vterm_t* t, uint8_t c) {
 
 void vterm_clear(vterm_t* t) {
   video_clear(t->video);
+  t->cattr = VGA_DEFAULT_ATTR;
   for (int row = 0; row < t->vheight; row++) {
     for (int col = 0; col < t->vwidth; col++) {
-      t->line_text[row][col] = ' ';
+      set_line_text(t, row, col, ' ', t->cattr);
     }
   }
 
@@ -149,7 +172,8 @@ void vterm_clear(vterm_t* t) {
 void vterm_redraw(vterm_t* t) {
   for (int row = 0; row < t->vheight; row++) {
     for (int col = 0; col < t->vwidth; col++) {
-      video_setc(t->video, row, col, t->line_text[row][col], VGA_DEFAULT_ATTR);
+      video_setc(t->video, row, col, line_text_c(t, row, col),
+                 line_text_attr(t, row, col));
     }
   }
   video_move_cursor(t->video, t->cursor_y, t->cursor_x);
