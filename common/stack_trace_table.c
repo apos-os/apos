03557b9b00a5d4276c61289c789cbac187b96e7c
Andrew Oates <andrew@andrewoates.com>
2015-01-08 23:44:25 -0500

    common: add stack_trace_table to store heap traces

---
+++ common/stack_trace_table.c
@@ -0,0 +1,97 @@
+// Copyright 2014 Andrew Oates.  All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "common/stack_trace_table.h"
+
+#include "common/kassert.h"
+#include "common/errno.h"
+#include "common/hash.h"
+#include "common/klog.h"
+#include "common/kstring.h"
+#include "common/math.h"
+#include "dev/interrupts.h"
+
+#define TRACETBL_ENTRIES 750
+
+typedef struct {
+  unsigned char len;
+  short refcount;
+  uint32_t hash;
+  addr_t trace[TRACETBL_MAX_TRACE_LEN];
+} entry_t;
+
+static entry_t g_tracetbl[TRACETBL_ENTRIES];
+static int g_tblsize = 0;
+
+int tracetbl_put(const addr_t* trace, int len) {
+  len = min(len, TRACETBL_MAX_TRACE_LEN);
+  const uint32_t trace_hash = fnv_hash_array(trace, sizeof(addr_t) * len);
+  int id = trace_hash % TRACETBL_ENTRIES;
+
+  PUSH_AND_DISABLE_INTERRUPTS();
+  KASSERT_DBG(g_tblsize >= 0 && g_tblsize <= TRACETBL_ENTRIES);
+  if (g_tblsize == TRACETBL_ENTRIES) {
+    klogfm(KL_GENERAL, DEBUG, "Stack trace table full; dropping trace @");
+    for (int i = 0; i < len; ++i) klogfm(KL_GENERAL, DEBUG, " %#x", trace[i]);
+    klogfm(KL_GENERAL, DEBUG, "\n");
+    POP_INTERRUPTS();
+    return -ENOMEM;
+  }
+
+  while (g_tracetbl[id].refcount > 0 && g_tracetbl[id].hash != trace_hash) {
+    id = (id + 1) % TRACETBL_ENTRIES;
+  }
+
+  if (g_tracetbl[id].refcount == 0) {
+    g_tracetbl[id].refcount = 1;
+    g_tracetbl[id].len = len;
+    g_tracetbl[id].hash = trace_hash;
+    for (int i = 0; i < len; ++i) g_tracetbl[id].trace[i] = trace[i];
+    g_tblsize++;
+  } else {
+    KASSERT_DBG(g_tracetbl[id].len == len);
+    KASSERT_DBG(kmemcmp(g_tracetbl[id].trace, trace, len) == 0);
+    g_tracetbl[id].refcount++;
+  }
+
+  POP_INTERRUPTS();
+  return id;
+}
+
+int tracetbl_get(int id, addr_t* trace) {
+  if (id < 0 || id >= TRACETBL_ENTRIES)
+    return -EINVAL;
+
+  PUSH_AND_DISABLE_INTERRUPTS();
+  int result;
+  if (g_tracetbl[id].len == 0) {
+    result = -EINVAL;
+  } else {
+    result = g_tracetbl[id].len;
+    for (int i = 0; i < result; ++i)
+      trace[i] = g_tracetbl[id].trace[i];
+  }
+  POP_INTERRUPTS();
+  return result;
+}
+
+void tracetbl_unref(int id) {
+  PUSH_AND_DISABLE_INTERRUPTS();
+  KASSERT(id >= 0 && id < TRACETBL_ENTRIES);
+  KASSERT(g_tracetbl[id].refcount > 0);
+  g_tracetbl[id].refcount--;
+  if (g_tracetbl[id].refcount == 0)
+    g_tblsize--;
+  POP_INTERRUPTS();
+}
---
+++ common/stack_trace_table.h
@@ -0,0 +1,44 @@
+// Copyright 2014 Andrew Oates.  All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// The stack trace table stores deduplicated stack traces, under the assumption
+// that many stack traces that are sampled for a particular reason will be the
+// same.
+//
+// Users record a stack trace with tracetbl_put(), which stores the stack trace
+// (or finds it if already stored) and returns an id it can be referenced with.
+// The client should call tracetbl_unref() when it no longer needs the stack
+// trace, to allow that space to be reused.
+#ifndef APOO_COMMON_STACK_TRACE_TABLE_H
+#define APOO_COMMON_STACK_TRACE_TABLE_H
+
+#include "common/types.h"
+
+#define TRACETBL_MAX_TRACE_LEN 16
+
+// Store the given stack trace, and return the id to fetch it later, or -error.
+// Increments the stack trace's refcount.
+int tracetbl_put(const addr_t* trace, int len);
+
+// Copy the stack trace referred to by the given id into |trace|.  Returns the
+// stack trace's length, or -error.  |trace| must be at least
+// TRACETBL_MAX_TRACE_LEN entries long.
+int tracetbl_get(int id, addr_t* trace);
+
+// Decrement the stack trace referred to by id's refcount.  If the refcount goes
+// to zero, the stacktrace slot may be reused (and any uses of that id are
+// invalid).
+void tracetbl_unref(int id);
+
+#endif
