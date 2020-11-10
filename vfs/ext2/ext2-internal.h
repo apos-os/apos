72d97f719b8ab860c0551cdbb7f078b241c90eb2
Andrew Oates <andrew@andrewoates.com>
2020-11-09 21:05:05 -0500

    ext2: mark superblock const-after-creation fields
    Ideally these could just be const, but I'm not sure that doesn't hit
    undefined behavior.  A future change might be to put these fields into a
    dedicated second struct that can be passed around independently of the
    ext2fs_t or superblock --- enforcing structurally that these fields
    aren't modified, and that code that depends on reading them without a
    lock held doesn't accidentally introduce a read on another field that is
    mutable.
    
    Some of these are set when the filesystem is initally created.  Others
    are in theory mutable, but we don't currently change them.

--- vfs/ext2/ext2-internal.h
+++ vfs/ext2/ext2-internal.h
@@ -21,35 +21,35 @@
 
 // ext2 superblock structure.
 typedef struct {
-  uint32_t s_inodes_count;
-  uint32_t s_blocks_count;
+  uint32_t s_inodes_count;  // const after creation
+  uint32_t s_blocks_count;  // const after creation
   uint32_t s_r_blocks_count;
   uint32_t s_free_blocks_count;
   uint32_t s_free_inodes_count;
-  uint32_t s_first_data_block;
-  uint32_t s_log_block_size;
-  uint32_t s_log_frag_size;
-  uint32_t s_blocks_per_group;
-  uint32_t s_frags_per_group;
-  uint32_t s_inodes_per_group;
+  uint32_t s_first_data_block;  // const after creation
+  uint32_t s_log_block_size;    // const after creation
+  uint32_t s_log_frag_size;     // const after creation
+  uint32_t s_blocks_per_group;  // const after creation
+  uint32_t s_frags_per_group;   // const after creation
+  uint32_t s_inodes_per_group;  // const after creation
   uint32_t s_mtime;
   uint32_t s_wtime;
   uint16_t s_mnt_count;
   uint16_t s_max_mnt_count;
-  uint16_t s_magic;
+  uint16_t s_magic;  // const after creation
   uint16_t s_state;
   uint16_t s_errors;
-  uint16_t s_minor_rev_level;
+  uint16_t s_minor_rev_level;  // const after creation
   uint32_t s_lastcheck;
   uint32_t s_checkinterval;
   uint32_t s_creator_os;
-  uint32_t s_rev_level;
+  uint32_t s_rev_level;  // const after creation
   uint16_t s_def_resuid;
   uint16_t s_def_resgid;
 
   // EXT2_DYNAMIC_REV Specific
-  uint32_t s_first_ino;
-  uint16_t s_inode_size;
+  uint32_t s_first_ino;   // const after creation
+  uint16_t s_inode_size;  // const after creation
   uint16_t s_block_group_nr;
   uint32_t s_feature_compat;
   uint32_t s_feature_incompat;
