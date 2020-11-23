5f01f28f81884be53e405f58485512def73b2e4f
Andrew Oates <andrew@andrewoates.com>
2020-11-23 17:27:52 -0500

    ramfs: allow VNODE_SOCKET in mknod

--- vfs/ramfs.c
+++ vfs/ramfs.c
@@ -317,7 +317,7 @@ int ramfs_lookup(vnode_t* parent, const char* name) {
 int ramfs_mknod(vnode_t* parent, const char* name,
                 vnode_type_t type, apos_dev_t dev) {
   KASSERT(type == VNODE_REGULAR || type == VNODE_BLOCKDEV ||
-          type == VNODE_CHARDEV || type == VNODE_FIFO);
+          type == VNODE_CHARDEV || type == VNODE_FIFO || type == VNODE_SOCKET);
   KASSERT(kstrcmp(parent->fstype, "ramfs") == 0);
   ramfs_t* ramfs = (ramfs_t*)parent->fs;
   maybe_block(parent->fs);
