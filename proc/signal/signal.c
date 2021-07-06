2f38bbd738f86089ad58352dc57e012cf6ed899d
Andrew Oates <andrew@andrewoates.com>
2021-07-06 12:30:02 -0400

    proc: add missing default IGNORE for SIGWINCH

--- proc/signal/signal.c
+++ proc/signal/signal.c
@@ -65,6 +65,7 @@ static signal_default_action_t kDefaultActions[APOS_SIGMAX + 1] = {
   SIGACT_TERM,          // SIGVTALRM
   SIGACT_TERM_AND_CORE, // SIGXCPU
   SIGACT_TERM_AND_CORE, // SIGXFSZ
+  SIGACT_IGNORE,        // SIGWINCH
 };
 
 ksigset_t proc_pending_signals(const process_t* proc) {
