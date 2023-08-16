00af5b128538718910af6a756fe799abcdde5db8
Andrew Oates <andrew@andrewoates.com>
2023-08-16 11:04:43 -0400

    vga: only call legacy IO functions when legacy VGA exists
    In practice the VGA code isn't called on non-VGA platforms anyway (i.e.
    riscv64), other than from the vterm tests.  We could just disable vterm
    tests on riscv64, but that logic isn't strongly tied to VGA or the
    underlying platform, so it's nicer to continue to be able to run/test
    it.
    
    It would be cleaner to make these virtual function calls against an
    abstract "video" interface, but IIRC this is a hotspot --- and this code
    isn't really safe or optimal anyway, so why put lipstick on the pig.

--- dev/video/vga.c
+++ dev/video/vga.c
@@ -46,10 +46,13 @@ void video_vga_init(void) {
   // TODO(aoates): support PCI-based VGA.
   KASSERT(ARCH_SUPPORTS_RAW_VGA);
   KASSERT(ARCH_SUPPORTS_LEGACY_PC_DEVS);
+
+#if ARCH_SUPPORTS_RAW_VGA
   // Make sure our CRT controller register is in "color" mode.
   uint8_t c = inb(MISC_OUTPUT_REG_READ);
   c |= MISC_OUTPUT_REG_IOAS;
   outb(MISC_OUTPUT_REG_WRITE, c);
+#endif
 
   video_show_cursor(NULL, true);
 }
@@ -69,6 +72,7 @@ void video_clear(video_t* v) {
 }
 
 void video_move_cursor(video_t* v, int row, int col) {
+#if ARCH_SUPPORTS_RAW_VGA
   const uint16_t cursor_pos = row * v->width + col;
 
   uint8_t orig_addr = inb(CRT_PORT_ADDR);
@@ -77,9 +81,11 @@ void video_move_cursor(video_t* v, int row, int col) {
   outb(CRT_PORT_ADDR, CRT_CURSOR_HIGH_ADDR);
   outb(CRT_PORT_DATA, (cursor_pos >> 8) & 0xFF);
   outb(CRT_PORT_ADDR, orig_addr);
+#endif
 }
 
 void video_show_cursor(video_t* v, bool visible) {
+#if ARCH_SUPPORTS_RAW_VGA
   uint8_t orig_addr = inb(CRT_PORT_ADDR);
   outb(CRT_PORT_ADDR, CRT_START_ADDR);
   uint8_t c = inb(CRT_PORT_DATA);
@@ -89,4 +95,5 @@ void video_show_cursor(video_t* v, bool visible) {
     c |= CRT_START_CURSOR_DISABLE;
   outb(CRT_PORT_DATA, c);
   outb(CRT_PORT_ADDR, orig_addr);
+#endif
 }
