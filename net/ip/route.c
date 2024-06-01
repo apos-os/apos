apos-os/apos

commit 4b62e895f086484597b982ed19d26329830a8138

net: support IPv6 default route in ip_route()

net/ip/route.c
@@ -33,8 +33,24 @@ typedef struct {
   const char* nic_name;
 } ip_route_rule_t;
 
-// TODO(aoates): support multiple default routes (e.g. ipv4 and ipv6).
-static ip_route_rule_t g_default_route;
+static ip_route_rule_t g_default_route_v4 = {.nexthop = {.family = AF_UNSPEC}};
+static ip_route_rule_t g_default_route_v6 = {.nexthop = {.family = AF_UNSPEC}};
+
+static ip_route_rule_t* get_default_route(addrfam_t family) {
+  switch (family) {
+    case ADDR_INET:
+      return &g_default_route_v4;
+      break;
+
+    case ADDR_INET6:
+      return &g_default_route_v6;
+      break;
+
+    case ADDR_UNSPEC:
+      break;
+  }
+  return NULL;
+}
 
 bool ip_route(netaddr_t dst, ip_routed_t* result) {
   // First try to find a NIC with a matching network.
@@ -84,9 +100,17 @@ bool ip_route(netaddr_t dst, ip_routed_t* result) {
 
   // No match, use the default route if we can.
   KASSERT_DBG(result->nic == NULL);
-  if (g_default_route.nexthop.family == dst.family) {
-    result->nexthop = g_default_route.nexthop;
-    result->nic = nic_get_nm(g_default_route.nic_name);
+  const ip_route_rule_t* route_rule = get_default_route(dst.family);
+  if (!route_rule) {
+    klogfm(KL_NET, DFATAL, "Invalid address family %d passed to ip_route()\n",
+           dst.family);
+    return false;
+  }
+
+  if (route_rule->nexthop.family != AF_UNSPEC) {
+    KASSERT(route_rule->nexthop.family == dst.family);
+    result->nexthop = route_rule->nexthop;
+    result->nic = nic_get_nm(route_rule->nic_name);
     if (result->nic) {
       result->src.family = ADDR_UNSPEC;
       kspin_lock(&result->nic->lock);
@@ -114,12 +138,31 @@ bool ip_route(netaddr_t dst, ip_routed_t* result) {
   return false;
 }
 
-void ip_set_default_route(netaddr_t nexthop, const char* nic_name) {
-  g_default_route.nexthop = nexthop;
-  g_default_route.nic_name = nic_name;
+void ip_set_default_route(addrfam_t family, netaddr_t nexthop,
+                          const char* nic_name) {
+  KASSERT(nexthop.family == AF_UNSPEC || nexthop.family == family);
+  ip_route_rule_t* route = get_default_route(family);
+  if (route) {
+    route->nexthop = nexthop;
+    route->nic_name = nic_name;
+  } else {
+    klogfm(KL_NET, DFATAL,
+           "Cannot set default route for invalid address family %d\n", family);
+  }
 }
 
-void ip_get_default_route(netaddr_t* nexthop, char* nic_name) {
-  *nexthop = g_default_route.nexthop;
-  kstrcpy(nic_name, g_default_route.nic_name);
+void ip_get_default_route(addrfam_t family, netaddr_t* nexthop,
+                          char* nic_name) {
+  const ip_route_rule_t* route = get_default_route(family);
+  if (route) {
+    *nexthop = route->nexthop;
+    if (route->nic_name) {
+      kstrcpy(nic_name, route->nic_name);
+    } else {
+      *nic_name = '\0';
+    }
+  } else {
+    klogfm(KL_NET, DFATAL,
+           "Cannot set default route for invalid address family %d\n", family);
+  }
 }

net/init.c
@@ -48,5 +48,5 @@ void net_init(void) {
   netaddr_t def;
   def.family = AF_INET;
   def.a.ip4.s_addr = str2inet("10.0.2.2");
-  ip_set_default_route(def, "eth0");
+  ip_set_default_route(ADDR_INET, def, "eth0");
 }

net/ip/route.h
@@ -30,9 +30,11 @@ typedef struct {
 // Find a route for the given destination address.
 bool ip_route(netaddr_t dst, ip_routed_t* result);
 
-// Set the default route.
+// Set the default route for the address's address family.  Pass an AF_UNSPEC
+// address to disable the default route for the given address family.
 // TODO(aoates): support arbitrary routing rules.
-void ip_set_default_route(netaddr_t nexthop, const char* nic_name);
-void ip_get_default_route(netaddr_t* nexthop, char* nic_name);
+void ip_set_default_route(addrfam_t family, netaddr_t nexthop,
+                          const char* nic_name);
+void ip_get_default_route(addrfam_t family, netaddr_t* nexthop, char* nic_name);
 
 #endif

test/net_base_test.c
@@ -444,11 +444,11 @@ static void route_default_route_test(test_fixture_t* t) {
   KTEST_BEGIN("ip_route(): no default route");
   netaddr_t orig_default_nexthop;
   char orig_default_nic[NIC_MAX_NAME_LEN];
-  ip_get_default_route(&orig_default_nexthop, orig_default_nic);
+  ip_get_default_route(ADDR_INET, &orig_default_nexthop, orig_default_nic);
 
   netaddr_t nexthop;
   nexthop.family = AF_UNSPEC;
-  ip_set_default_route(nexthop, "");
+  ip_set_default_route(ADDR_INET, nexthop, "");
 
   netaddr_t dst;
   kmemset(&dst, 0xcd, sizeof(dst));
@@ -463,21 +463,21 @@ static void route_default_route_test(test_fixture_t* t) {
 
   KTEST_BEGIN("ip_route(): no default route (with NIC name)");
   nexthop.family = AF_UNSPEC;
-  ip_set_default_route(nexthop, t->nic->name);
+  ip_set_default_route(ADDR_INET, nexthop, t->nic->name);
   KEXPECT_FALSE(ip_route(dst, &result));
 
 
   KTEST_BEGIN("ip_route(): default route with bad NIC name");
   nexthop.family = AF_INET;
   nexthop.a.ip4.s_addr = str2inet("1.2.3.4");
-  ip_set_default_route(nexthop, "not_a_nic");
+  ip_set_default_route(ADDR_INET, nexthop, "not_a_nic");
   KEXPECT_FALSE(ip_route(dst, &result));
 
 
   KTEST_BEGIN("ip_route(): default route set");
   nexthop.family = AF_INET;
   nexthop.a.ip4.s_addr = str2inet("1.2.3.4");
-  ip_set_default_route(nexthop, t->nic->name);
+  ip_set_default_route(ADDR_INET, nexthop, t->nic->name);
   kmemset(&result, 0xab, sizeof(result));
   KEXPECT_TRUE(ip_route(dst, &result));
   KEXPECT_EQ(t->nic, result.nic);
@@ -491,11 +491,69 @@ static void route_default_route_test(test_fixture_t* t) {
   KTEST_BEGIN("ip_route(): default route set (no usable addrs on NIC)");
   nexthop.family = AF_INET;
   nexthop.a.ip4.s_addr = str2inet("1.2.3.4");
-  ip_set_default_route(nexthop, t->nic3->name);
+  ip_set_default_route(ADDR_INET, nexthop, t->nic3->name);
   kmemset(&result, 0xab, sizeof(result));
   KEXPECT_FALSE(ip_route(dst, &result));
 
-  ip_set_default_route(orig_default_nexthop, orig_default_nic);
+  ip_set_default_route(ADDR_INET, orig_default_nexthop, orig_default_nic);
+}
+
+static void route_default_route_v6_test(test_fixture_t* t) {
+  KTEST_BEGIN("ip_route(): no default route (IPv6)");
+  netaddr_t orig_default_nexthop;
+  char orig_default_nic[NIC_MAX_NAME_LEN];
+  ip_get_default_route(ADDR_INET6, &orig_default_nexthop, orig_default_nic);
+
+  netaddr_t nexthop;
+  nexthop.family = AF_UNSPEC;
+  ip_set_default_route(ADDR_INET6, nexthop, "");
+
+  netaddr_t dst;
+  kmemset(&dst, 0xcd, sizeof(dst));
+  dst.family = AF_INET6;
+  KEXPECT_EQ(0, str2inet6("2607:f8b0:4006:821::2004", &dst.a.ip6));
+
+  char addr[INET6_PRETTY_LEN];
+  ip_routed_t result;
+  kmemset(&result, 0xab, sizeof(result));
+  KEXPECT_FALSE(ip_route(dst, &result));
+
+
+  KTEST_BEGIN("ip_route(): no default route (with NIC name) (IPv6)");
+  nexthop.family = AF_UNSPEC;
+  ip_set_default_route(ADDR_INET6, nexthop, t->nic->name);
+  KEXPECT_FALSE(ip_route(dst, &result));
+
+
+  KTEST_BEGIN("ip_route(): default route with bad NIC name (IPv6)");
+  nexthop.family = AF_INET6;
+  KEXPECT_EQ(0, str2inet6("2001:db8::100", &nexthop.a.ip6));
+  ip_set_default_route(ADDR_INET6, nexthop, "not_a_nic");
+  KEXPECT_FALSE(ip_route(dst, &result));
+
+
+  KTEST_BEGIN("ip_route(): default route set (IPv6)");
+  nexthop.family = AF_INET6;
+  KEXPECT_EQ(0, str2inet6("2001:db8::100", &nexthop.a.ip6));
+  ip_set_default_route(ADDR_INET6, nexthop, t->nic->name);
+  kmemset(&result, 0xab, sizeof(result));
+  KEXPECT_TRUE(ip_route(dst, &result));
+  KEXPECT_EQ(t->nic, result.nic);
+  KEXPECT_EQ(AF_INET6, result.src.family);
+  KEXPECT_STREQ("2001:db8::1", inet62str(&result.src.a.ip6, addr));
+  KEXPECT_EQ(AF_INET6, result.nexthop.family);
+  KEXPECT_STREQ("2001:db8::100", inet62str(&result.nexthop.a.ip6, addr));
+  nic_put(result.nic);
+
+
+  KTEST_BEGIN("ip_route(): default route set (no usable addrs on NIC) (IPv6)");
+  nexthop.family = AF_INET6;
+  KEXPECT_EQ(0, str2inet6("2001:db8::100", &nexthop.a.ip6));
+  ip_set_default_route(ADDR_INET6, nexthop, t->nic2->name);
+  kmemset(&result, 0xab, sizeof(result));
+  KEXPECT_FALSE(ip_route(dst, &result));
+
+  ip_set_default_route(ADDR_INET6, orig_default_nexthop, orig_default_nic);
 }
 
 static void ip_route_tests(test_fixture_t* t) {
@@ -503,6 +561,7 @@ static void ip_route_tests(test_fixture_t* t) {
   route_longest_prefix_test(t);
   route_longest_prefix_v6_test(t);
   route_default_route_test(t);
+  route_default_route_v6_test(t);
 }
 
 // TODO(aoates): neighbor cache tests:

commit 6286362e3eebfd196c7957fe7318314177c07401

net: fix use-after-free by storing full NIC name in routes
The bug was introduced in 4b62e895f086484597; storing just a string pointer
doesn't work when passing buffers back and forth.  Just store the whole string
now (what really this code should have done from the start).

net/ip/route.c
@@ -30,7 +30,7 @@ typedef struct {
 
   // The device to use.
   // TODO(aoates): make this optional (so we can use nexthop alone).
-  const char* nic_name;
+  char nic_name[NIC_MAX_NAME_LEN];
 } ip_route_rule_t;
 
 static ip_route_rule_t g_default_route_v4 = {.nexthop = {.family = AF_UNSPEC}};
@@ -141,10 +141,11 @@ bool ip_route(netaddr_t dst, ip_routed_t* result) {
 void ip_set_default_route(addrfam_t family, netaddr_t nexthop,
                           const char* nic_name) {
   KASSERT(nexthop.family == AF_UNSPEC || nexthop.family == family);
+  KASSERT(kstrlen(nic_name) < NIC_MAX_NAME_LEN);
   ip_route_rule_t* route = get_default_route(family);
   if (route) {
     route->nexthop = nexthop;
-    route->nic_name = nic_name;
+    kstrcpy(route->nic_name, nic_name);
   } else {
     klogfm(KL_NET, DFATAL,
            "Cannot set default route for invalid address family %d\n", family);
@@ -154,13 +155,12 @@ void ip_set_default_route(addrfam_t family, netaddr_t nexthop,
 void ip_get_default_route(addrfam_t family, netaddr_t* nexthop,
                           char* nic_name) {
   const ip_route_rule_t* route = get_default_route(family);
+  KASSERT(route->nexthop.family == AF_UNSPEC ||
+          route->nexthop.family == family);
+  KASSERT(kstrlen(route->nic_name) < NIC_MAX_NAME_LEN);
   if (route) {
     *nexthop = route->nexthop;
-    if (route->nic_name) {
-      kstrcpy(nic_name, route->nic_name);
-    } else {
-      *nic_name = '\0';
-    }
+    kstrcpy(nic_name, route->nic_name);
   } else {
     klogfm(KL_NET, DFATAL,
            "Cannot set default route for invalid address family %d\n", family);