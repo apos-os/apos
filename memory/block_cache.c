apos-os/apos

commit a3f32e492c0d75a14821a129ff3c3f13fa3e0f9d

memory: track list of BC entries in memobj
This allows at least two interesting things,
1) finding all bc_entry_ts associated with a memobj and
   force-flush/force-free them (e.g. during a filesystem unmount)
2) finding all bc_entry_ts in a shadow object to migrate them during
   shadow chain collapse.

memory/block_cache.c
@@ -61,6 +61,7 @@ typedef struct bc_entry_internal {
   // Link on the flush queue and LRU queue.
   list_link_t flushq;
   list_link_t lruq;
+  list_link_t memobj_link;
 
   // Set to true when the entry is flushed to disk, and to false when the entry
   // is taken by a thread.
@@ -280,6 +281,9 @@ static void free_dead_entries(void) {
 
     KASSERT_DBG(entry->initialized == false);
     KASSERT_DBG(entry->pin_count == 0);
+    list_remove(&entry->pub.obj->bc_entries, &entry->memobj_link);
+    entry->pub.obj->num_bc_entries--;
+    KASSERT_DBG(entry->pub.obj->num_bc_entries >= 0);
     entry->pub.obj->ops->unref(entry->pub.obj);  // May block.
     entry->pub.obj = 0x0;
     kfree(entry);
@@ -427,10 +431,14 @@ int block_cache_get(memobj_t* obj, int offset, bc_entry_t** entry_out) {
     entry->flushing = false;
     entry->flushq = LIST_LINK_INIT;
     entry->lruq = LIST_LINK_INIT;
+    entry->memobj_link = LIST_LINK_INIT;
     kthread_queue_init(&entry->wait_queue);
 
     // Put the uninitialized entry into the table.
     htbl_put(&g_table, h, entry);
+    KASSERT_DBG(obj->num_bc_entries >= 0);
+    list_push(&obj->bc_entries, &entry->memobj_link);
+    obj->num_bc_entries++;
 
     // Unlock mutex around reentrant and blocking operations.
     kmutex_unlock(&g_mu);

memory/memobj.c
@@ -19,4 +19,6 @@ void memobj_base_init(memobj_t* obj) {
   kmemset(obj, 0, sizeof(memobj_t));
   obj->lock = KSPINLOCK_NORMAL_INIT;
   obj->refcount = 1;
+  obj->bc_entries = LIST_INIT;
+  obj->num_bc_entries = 0;
 }

memory/memobj.h
@@ -48,6 +48,10 @@ typedef struct memobj {
   int refcount;
   kspinlock_t lock;
 
+  // List of extant block cache entries.  Maintained by the block cache code.
+  list_t bc_entries;
+  int num_bc_entries;
+
   // Data specific to the type memory object.
   void* data;
 } memobj_t;

commit fac786865b1b5929aecad7b9f9dc3ede09836cfa

block cache: fix logical and data race when freeing dead entries
I got _extraordinarily_ lucky triggering this --- it happened to show up during
a test run as a crash on an assertion in list.c that could only be explained by
corruption of the memobj's bc_entries list.  I was not able to repro until I
tried a version where I inserted artificial delays (with busy-loops) at many
spots in the list code to increase the chance of a preemption occurring in the
middle of the list operation.

Artificial repro example: sponge-20241111-131808
Thread 51 (crashing):
 #0 0xffffffffc010f1dd in die() [kassert.c:57]
 #1 0xffffffffc010f367 in kassert_msg() [kassert.c:72]
 #2 0xffffffffc0467110 in list_remove() [list.c:101]
 #3 0xffffffffc011b58e in free_dead_entries() [block_cache.c:315]
 #4 0xffffffffc0119e39 in maybe_free_cache_space() [block_cache.c:377]
 #5 0xffffffffc01197dd in block_cache_get() [block_cache.c:517]
 #6 0xffffffffc04ccf97 in multithread_pressure_test_worker() [block_cache_test.c:556]
 #7 0xffffffffc013c9cc in kthread_trampoline() [kthread.c:82]
PANIC: assertion failed: list->tail->next == link (common/list.c:101)

Original repro (clean): sponge-20241109-184508-x86_64-clang-0025
Thread 1623 (crashing):
 #0 0xffffffffc010f1dd in die() [kassert.c:57]
 #1 0xffffffffc010f367 in kassert_msg() [kassert.c:72]
 #2 0xffffffffc0466b4b in list_pop() [list.c:43]
 #3 0xffffffffc0466e4f in list_remove() [list.c:86]
 #4 0xffffffffc011b58e in free_dead_entries() [block_cache.c:315]
 #5 0xffffffffc0119e39 in maybe_free_cache_space() [block_cache.c:377]
 #6 0xffffffffc01197dd in block_cache_get() [block_cache.c:517]
 #7 0xffffffffc04ccdc7 in multithread_pressure_test_worker() [block_cache_test.c:554]
 #8 0xffffffffc013c86c in kthread_trampoline() [kthread.c:82]
PANIC: assertion failed: link->prev == 0x0 (common/list.c:43)

Weirdly, in both these cases, it doesn't seem that any of the other block cache
threads have been preempted --- this may mean an indirect issue.  Thread A is
preempted mid-op; thread B corrupts the list, but neither A nor B notice; thread
C detects corruption.

The bug here was introduced in 2021 in commit a3f32e492, which added the new

memory/block_cache.c
@@ -308,13 +308,14 @@ static void free_dead_entries(void) {
   kmutex_assert_is_held(&g_mu);
   while (!list_empty(&g_cleanup_list)) {
     bc_entry_internal_t* entry = cache_entry_pop(&g_cleanup_list, lruq);
-    kmutex_unlock(&g_mu);
 
     KASSERT_DBG(entry->initialized == false);
     KASSERT_DBG(entry->pin_count == 0);
     list_remove(&entry->pub.obj->bc_entries, &entry->memobj_link);
     entry->pub.obj->num_bc_entries--;
     KASSERT_DBG(entry->pub.obj->num_bc_entries >= 0);
+    kmutex_unlock(&g_mu);
+
     entry->pub.obj->ops->unref(entry->pub.obj);  // May block.
     entry->pub.obj = 0x0;
     kfree(entry);