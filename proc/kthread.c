apos-os/apos

commit f5ed94769fdace1f21617073313d20ee50a8a691

proc: protect thread state with spinlock
What we've been working towards.

This is mostly straightforward, but requires some shenanigans with join/exit,
which are currently both logically- and data-racy.  Now join_list is properly
protected with the spinlock, and kthread_join() atomically checks the target
thread's state and waits until it has exited.

In an SMP context it's possible for a thread that is currently running to be
selected to run on another core before it yields; that will need to be handled.

proc/kthread.c
@@ -140,6 +140,7 @@ void kthread_init(void) {
   kthread_data_t* first = (kthread_data_t*)kmalloc(sizeof(kthread_data_t));
   KASSERT(first != 0x0);
   kthread_init_kthread(first);
+  kspin_int_constructor(&first->spin);
   first->state = KTHREAD_RUNNING;
   first->id = g_next_id++;
   first->stack = (addr_t*)get_global_meminfo()->thread0_stack.base;
@@ -173,6 +174,7 @@ int kthread_create(kthread_t* thread_ptr, void* (*start_routine)(void*),
   *thread_ptr = thread;
 
   kthread_init_kthread(thread);
+  kspin_int_constructor(&thread->spin);
   thread->id = g_next_id++;
   thread->state = KTHREAD_PENDING;
   thread->retval = 0x0;
@@ -227,6 +229,7 @@ int kthread_create(kthread_t* thread_ptr, void* (*start_routine)(void*),
 }
 
 void kthread_destroy(kthread_t thread) {
+  kspin_int_destructor(&thread->spin);
   KASSERT(thread->state == KTHREAD_DONE);
   KASSERT(refcount_get(&thread->ref) == 0);
   PUSH_AND_DISABLE_INTERRUPTS();
@@ -257,16 +260,30 @@ void kthread_detach(kthread_t thread_ptr) {
 
 void* kthread_join(kthread_t thread_ptr) {
   kthread_data_t* thread = thread_ptr;
+  PUSH_AND_DISABLE_INTERRUPTS();
+  // Note: we cast this to a kspinlock_t since the scheduler doesn't support
+  // waiting on an interrupt-safe spinlock.  This is safe because,
+  //  1) interrupt-safe spinlocks are strictly more protective than normal ones
+  //  2) we never access join_list from an interrupt context
+  //  3) interrupts are already disabled anyway
+  // Therefore, any simultaneous accesses to join_list (or other thread state)
+  // must be happening on another SMP core, and therefore protected by the
+  // actual spinning part of the spinlock.
+  kspin_lock((kspinlock_t*)&thread->spin);
   KASSERT(thread->state == KTHREAD_PENDING ||
           thread->state == KTHREAD_DONE);
 
   if (thread->state != KTHREAD_DONE) {
-    scheduler_wait_on(&thread->join_list);
+    scheduler_wait_on_splocked(&thread->join_list, -1,
+                               (kspinlock_t*)&thread->spin);
   }
+  KASSERT(thread->state == KTHREAD_DONE);
+  kspin_unlock((kspinlock_t*)&thread->spin);
+  POP_INTERRUPTS();
+
 #if ENABLE_TSAN
   tsan_thread_join(thread_ptr);
 #endif
-  KASSERT(thread->state == KTHREAD_DONE);
   void* retval = thread->retval;
   // Return our reference.  This will free the thread if we're last.
   kthread_unref(thread);
@@ -274,30 +291,34 @@ void* kthread_join(kthread_t thread_ptr) {
 }
 
 bool kthread_is_done(kthread_t thread) {
-  return thread->state == KTHREAD_DONE;
+  kspin_lock_int(&thread->spin);
+  bool result = thread->state == KTHREAD_DONE;
+  kspin_unlock_int(&thread->spin);
+  return result;
 }
 
 void kthread_exit(void* x) {
-  PUSH_AND_DISABLE_INTERRUPTS();
-  KASSERT(PER_CPU(g_current_thread)->spinlocks_held == 0);
-  KASSERT(PER_CPU(g_current_thread)->process == NULL);
+  kthread_t thread = PER_CPU(g_current_thread);
+  kspin_lock_int(&thread->spin);
+  KASSERT(thread->spinlocks_held == 1);
+  KASSERT(thread->process == NULL);
 
   // kthread_exit is basically the same as kthread_yield, but we don't put
   // ourselves back on the run queue.
-  PER_CPU(g_current_thread)->retval = x;
-  PER_CPU(g_current_thread)->state = KTHREAD_DONE;
+  thread->retval = x;
+  thread->state = KTHREAD_DONE;
 
   // Schedule all the waiting threads.
-  scheduler_wake_all(&PER_CPU(g_current_thread)->join_list);
+  scheduler_wake_all(&thread->join_list);
+  kspin_unlock_int(&thread->spin);
 
   // Transfer our reference to the reap queue.
-  kthread_queue_push(&g_reap_queue, PER_CPU(g_current_thread));
+  kthread_queue_push(&g_reap_queue, thread);
 
   scheduler_yield_no_reschedule();
 
   // Never get here!
   KASSERT(0);
-  POP_INTERRUPTS();
 }
 
 void kthread_run_on_all(void (*f)(kthread_t, void*), void* arg) {

proc/kthread-internal.h
@@ -48,7 +48,7 @@ struct kthread_data {
   kthread_arch_context_t context;
   // TODO(aoates): protect more thread state with the spinlock.
   kspinlock_intsafe_t spin;
-  kthread_state_t state;
+  kthread_state_t state GUARDED_BY(&spin);
   void* retval;
   struct kthread_data* prev;
   struct kthread_data* next;
@@ -56,7 +56,7 @@ struct kthread_data {
   addr_t* stack;  // The block of memory allocated for the thread's stack.
   addrdiff_t stacklen;
   atomic32_t runnable;  // Crude way to disable threads for tests.
-  kthread_queue_t join_list;  // List of thread's join()'d to this one.
+  kthread_queue_t join_list GUARDED_BY(&spin);  // List of thread's join()'d to this one.
   process_t* process;  // The process owning this thread.
   analysis_lock_t process_spin_mu;  // Shadows process->spin_mu
 

proc/scheduler.c
@@ -57,9 +57,12 @@ static tsan_lock_data_t g_implicit_scheduler_tsan_lock;
 #endif
 
 static void* idle_thread_body(void* arg) {
+  kthread_t me = kthread_current_thread();
   sched_disable_preemption();
   while(1) {
-    kthread_current_thread()->state = KTHREAD_PENDING;
+    kspin_lock_int(&me->spin);
+    me->state = KTHREAD_PENDING;
+    kspin_unlock_int(&me->spin);
     scheduler_yield_no_reschedule();
   }
   return 0;
@@ -121,6 +124,7 @@ void scheduler_yield_no_reschedule(void) {
   PUSH_AND_DISABLE_INTERRUPTS();
   raw_spin_lock(&g_run_queue.spin);
   kthread_data_t* new_thread = scheduler_pick_next(&g_run_queue, true);
+  // TODO(aoates): handle threads that are still running and about to yield.
   // Note: this is racey with changes in runnable --- that is OK.  If we get a
   // non-runnable thread here, assume that there _are_ no runnable threads.
   if (new_thread && atomic_load_relaxed(&new_thread->runnable)) {

test/kthread_test.c
@@ -822,8 +822,15 @@ static void scheduler_interrupt_timeout_test(void) {
   }
 }
 
+static kthread_state_t thread_state(kthread_t t) {
+  kspin_lock_int(&t->spin);
+  kthread_state_t result = t->state;
+  kspin_unlock_int(&t->spin);
+  return result;
+}
+
 static void* kthread_is_done_test_helper(void* arg) {
-  KEXPECT_EQ(KTHREAD_RUNNING, kthread_current_thread()->state);
+  KEXPECT_EQ(KTHREAD_RUNNING, thread_state(kthread_current_thread()));
   KEXPECT_EQ(false, kthread_is_done(kthread_current_thread()));
   scheduler_wait_on((kthread_queue_t*)arg);
   return 0x0;
@@ -835,22 +842,22 @@ static void kthread_is_done_test(void) {
   kthread_queue_t queue;
   kthread_queue_init(&queue);
   KEXPECT_EQ(0, kthread_create(&thread, &kthread_is_done_test_helper, &queue));
-  KEXPECT_EQ(KTHREAD_PENDING, thread->state);
+  KEXPECT_EQ(KTHREAD_PENDING, thread_state(thread));
   KEXPECT_EQ(false, kthread_is_done(thread));
 
   KTEST_BEGIN("kthread_is_done(): scheduled thread");
   scheduler_make_runnable(thread);
-  KEXPECT_EQ(KTHREAD_PENDING, thread->state);
+  KEXPECT_EQ(KTHREAD_PENDING, thread_state(thread));
   KEXPECT_EQ(false, kthread_is_done(thread));
 
   KTEST_BEGIN("kthread_is_done(): blocked-on-queue thread");
   while (get_queue(thread) != &queue) scheduler_yield();
-  KEXPECT_EQ(KTHREAD_PENDING, thread->state);
+  KEXPECT_EQ(KTHREAD_PENDING, thread_state(thread));
   KEXPECT_EQ(false, kthread_is_done(thread));
 
   KTEST_BEGIN("kthread_is_done(): finished thread");
   scheduler_wake_all(&queue);
-  while (thread->state != KTHREAD_DONE) scheduler_yield();
+  while (thread_state(thread) != KTHREAD_DONE) scheduler_yield();
   KEXPECT_EQ(true, kthread_is_done(thread));
 
   KEXPECT_EQ(NULL, kthread_join(thread));

commit 7d74f97ec86bc2ac96cfc8ab973c1413a5cbc101

kthread: fix preemption race on thread exit
There is a race if a thread is preempted after it starts to exit.  This can
cause various problems, including:
 * panicing in a joining thread that sees the exiting thread in KTHREAD_PENDING
   (after it yields when preempeted)
 * panicing when preemption is started because the thread is found on the reap
   queue

This bug was introduced in f5ed94769 ("proc: protect thread state with
spinlock"), which replaced the PUSH_AND_DISABLE_INTERRUPTS() block with a
spinlock critical section --- critically, this causes interrupts to be
re-enabled after the spinlock is unlocked, rather than stay disabled through the
end of the thread exit.

I'm choosing to disable preemption here rather than interrupts because it's a
narrower fix --- in SMP, other threads and interrupts can still observe the
exiting thread (and other state), so only disabling preemption more closely
emulates that even when running without SMP.

Example stack trace showing crash:
Thread 1402:
 #0 0xc012e4ed in kthread_switch() [kthread.c:433]
 #1 0xc012f6d5 in scheduler_yield_no_reschedule() [scheduler.c:174]
 #2 0xc0130081 in scheduler_wait() [scheduler.c:335]
 #3 0xc0130160 in scheduler_wait_on() [scheduler.c:362]
 #4 0xc012f431 in scheduler_yield() [scheduler.c:123]
 #5 0xc0130429 in sched_tick() [scheduler.c:438]
 #6 0xc038ce61 in internal_timer_handler() [timer.c:127]
 #7 0xc038691f in irq_handler() [irq.c:130]
 #8 0xc03862c0 in int_handler() [interrupts.c:241]
 #9 0xc0386b97 in int_common_handler() [isr.s:127]
 #10 0xc0386a8b in int32() [isr.s:77]
 #11 0xc03863da in restore_interrupts() [interrupts.c:302]
 #12 0xc0131b3e in kspin_unlock_int() [spinlock.c:106]
 #13 0xc012e0fb in kthread_exit() [kthread.c:325]
 #14 0xc012d855 in kthread_trampoline() [kthread.c:135]
...
Thread 8 (crashing):
 #0 0xc010820c in die() [kassert.c:58]
 #1 0xc0108293 in kassert_msg() [kassert.c:73]
 #2 0xc012df51 in kthread_join() [kthread.c:292 (discriminator 4)]

proc/kthread.c
@@ -310,6 +310,10 @@ bool kthread_is_done(kthread_t thread) {
 }
 
 void kthread_exit(void* x) {
+  // Disable preemption permanently --- we don't want to be preempted after this
+  // point, we want to run until we finish exiting.
+  sched_disable_preemption();
+
   kthread_t thread = PER_CPU(g_current_thread);
   kspin_lock_int(&thread->spin);
   KASSERT(thread->spinlocks_held == 1);