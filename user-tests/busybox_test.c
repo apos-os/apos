210c466c11b6e9913a4e3b0b353a22f2fcec05ea
Andrew Oates <andrew@andrewoates.com>
2025-11-01 14:45:16 -0400

    user-tests: add more single-file busybox tests (wc, hash/checksum utils)

--- user-tests/busybox_test.c
+++ user-tests/busybox_test.c
@@ -13,6 +13,7 @@
 // limitations under the License.
 //
 // A test suite that exercises basic busybox functionality.
+#include <assert.h>
 #include <ctype.h>
 #include <sys/unistd.h>
 
@@ -22,6 +23,20 @@
 #define BUF_SIZE 2000
 #define STDOUT_FILE "_bbtest_stdout.txt"
 #define STDERR_FILE "_bbtest_stderr.txt"
+#define TEST_FILE "_bbtest_data.txt"
+
+const char kTestFileData[] =
+    "ACT I\n"
+    "\n"
+    "SCENE I. Elsinore. A platform before the castle.\n"
+    "\n"
+    "FRANCISCO at his post. Enter to him BERNARDO\n"
+    "BERNARDO\n"
+    "Who's there?\n"
+    "FRANCISCO\n"
+    "Nay, answer me: stand, and unfold yourself.\n"
+    "BERNARDO\n"
+    "Long live the king!\n";
 
 typedef struct {
   int status;
@@ -86,6 +101,25 @@ static char* stripr(char* str) {
   return str;
 }
 
+static void setup_busybox_tests(void) {
+  KTEST_BEGIN("busybox: test setup");
+  int fd = open(TEST_FILE, O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU);
+  const char* buf = kTestFileData;
+  int bytes_left = strlen(buf);
+  while (bytes_left > 0) {
+    ssize_t written = write(fd, buf, bytes_left);
+    assert(written >= 0);
+    buf += written;
+    bytes_left -= written;
+  }
+  close(fd);
+}
+
+static void cleanup_busybox_tests(void) {
+  KTEST_BEGIN("busybox: test cleanup");
+  KEXPECT_EQ(0, unlink(TEST_FILE));
+}
+
 static void ascii_test(void) {
   KTEST_BEGIN("busybox: ascii test");
   cmd_result_t res;
@@ -113,6 +147,97 @@ static void ascii_test(void) {
     );
 }
 
+static void cat_test(void) {
+  KTEST_BEGIN("busybox: cat test");
+  cmd_result_t res;
+  KEXPECT_EQ(0, run_bb((const char*[])
+                       {"cat", TEST_FILE, NULL},
+                       &res));
+  KEXPECT_MULTILINE_STREQ(res.out, kTestFileData);
+}
+
+// Hash/checksum tests.  Generated with the following script:
+//   COMMANDS=(cksum md5sum sha1sum sha256sum sha3sum sha512sum)
+//
+//   for cmd in ${COMMANDS[@]}; do
+//     output=$($cmd /tmp/test.txt | sed 's,/tmp/test.txt,,g')
+//     out=$(cat <<EOF
+//   static void ${cmd}_test(void) {
+//     KTEST_BEGIN("busybox: $cmd test");
+//     cmd_result_t res;
+//     KEXPECT_EQ(0, run_bb((const char*[])
+//                          {"$cmd", TEST_FILE, NULL},
+//                          &res));
+//     KEXPECT_STREQ(stripr(res.out), "$output" TEST_FILE);
+//   }
+//   EOF
+//   )
+//     echo "$out"
+//     echo
+//   done
+static void cksum_test(void) {
+  KTEST_BEGIN("busybox: cksum test");
+  cmd_result_t res;
+  KEXPECT_EQ(0, run_bb((const char*[])
+                       {"cksum", TEST_FILE, NULL},
+                       &res));
+  KEXPECT_STREQ(stripr(res.out), "1281999004 207 " TEST_FILE);
+}
+
+static void md5sum_test(void) {
+  KTEST_BEGIN("busybox: md5sum test");
+  cmd_result_t res;
+  KEXPECT_EQ(0, run_bb((const char*[])
+                       {"md5sum", TEST_FILE, NULL},
+                       &res));
+  KEXPECT_STREQ(stripr(res.out), "52113b950f5ebcc7b32a469a051e489b  " TEST_FILE);
+}
+
+static void sha1sum_test(void) {
+  KTEST_BEGIN("busybox: sha1sum test");
+  cmd_result_t res;
+  KEXPECT_EQ(0, run_bb((const char*[])
+                       {"sha1sum", TEST_FILE, NULL},
+                       &res));
+  KEXPECT_STREQ(stripr(res.out), "6f1f90197497cb1aeb975189084612588630a1a0  " TEST_FILE);
+}
+
+static void sha256sum_test(void) {
+  KTEST_BEGIN("busybox: sha256sum test");
+  cmd_result_t res;
+  KEXPECT_EQ(0, run_bb((const char*[])
+                       {"sha256sum", TEST_FILE, NULL},
+                       &res));
+  KEXPECT_STREQ(stripr(res.out), "6d89c5a362b001906adae6d5a0d40bd6c91ac6a85823f92ea20bc5cbf0c2f155  " TEST_FILE);
+}
+
+static void sha3sum_test(void) {
+  KTEST_BEGIN("busybox: sha3sum test");
+  cmd_result_t res;
+  KEXPECT_EQ(0, run_bb((const char*[])
+                       {"sha3sum", TEST_FILE, NULL},
+                       &res));
+  KEXPECT_STREQ(stripr(res.out), "a360bfdf087dda0109dfee3e5534ca919411ad2698fcf7ce2bd67042  " TEST_FILE);
+}
+
+static void sha512sum_test(void) {
+  KTEST_BEGIN("busybox: sha512sum test");
+  cmd_result_t res;
+  KEXPECT_EQ(0, run_bb((const char*[])
+                       {"sha512sum", TEST_FILE, NULL},
+                       &res));
+  KEXPECT_STREQ(stripr(res.out), "840566c75ce33ca99c02b303a9e627be1d3a368f32fb386681d52c7aa192cf6af00dd7f281dda22a6cbe5ba1cde8a35ef08d02a9eb3c68e4bc87880d679de70b  " TEST_FILE);
+}
+
+static void crc32_test(void) {
+  KTEST_BEGIN("busybox: crc32 test");
+  cmd_result_t res;
+  KEXPECT_EQ(0, run_bb((const char*[])
+                       {"crc32", TEST_FILE, NULL},
+                       &res));
+  KEXPECT_STREQ(stripr(res.out), "88ec64a2 " TEST_FILE);
+}
+
 static void date_test(void) {
   KTEST_BEGIN("busybox: date test");
   cmd_result_t res;
@@ -127,8 +252,32 @@ static void date_test(void) {
   KEXPECT_STREQ(stripr(res.err), "date: invalid date '-1234'");
 }
 
+static void wc_test(void) {
+  KTEST_BEGIN("busybox: wc test");
+  cmd_result_t res;
+  KEXPECT_EQ(0, run_bb((const char*[])
+                       {"wc", TEST_FILE, NULL},
+                       &res));
+  KEXPECT_STREQ(stripr(res.out), "       11        34       207 " TEST_FILE);
+}
+
 void busybox_tests(void) {
   KTEST_SUITE_BEGIN("busybox tests");
+  setup_busybox_tests();
+
   ascii_test();
+  cat_test();
   date_test();
+  wc_test();
+
+  // Hash function tests.
+  cksum_test();
+  crc32_test();
+  md5sum_test();
+  sha1sum_test();
+  sha256sum_test();
+  sha3sum_test();
+  sha512sum_test();
+
+  cleanup_busybox_tests();
 }
